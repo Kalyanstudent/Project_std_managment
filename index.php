3af9329e50689a21827d5e283a70d8692a936970
Kalyanstudent <141897830+Kalyanstudent@users.noreply.github.com>
2024-09-22 11:51:46 -0500

    Update index.php

--- index.php
+++ index.php
@@ -66,6 +66,33 @@
                 float: none;
             }
         }
+
+        .container {
+            display: inline-block;
+            cursor: pointer;
+        }
+
+        .bar1,
+        .bar2,
+        .bar3 {
+            width: 35px;
+            height: 5px;
+            background-color: #333;
+            margin: 6px 0;
+            transition: 0.4s;
+        }
+
+        .change .bar1 {
+            transform: translate(0, 11px) rotate(-45deg);
+        }
+
+        .change .bar2 {
+            opacity: 0;
+        }
+
+        .change .bar3 {
+            transform: translate(0, -11px) rotate(45deg);
+        }
     </style>
 </head>
 
@@ -77,13 +104,12 @@
             <a class="active" href="#home">Home</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
-            <a href="#">
                 <div class="container" onclick="myFunction(this)">
                     <div class="bar1"></div>
                     <div class="bar2"></div>
                     <div class="bar3"></div>
                 </div>
-            </a>
+       
         </div>
     </div>
 
@@ -99,7 +125,7 @@
 
 
 <script>
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+    function myFunction(x) {
+        x.classList.toggle("change");
+    }
 </script>
